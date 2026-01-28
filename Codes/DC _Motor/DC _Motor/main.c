f86e84f724a1c56277383f79182e9becf2ef361a
Farseena CK <farseenack515@gmail.com>
2026-01-28 19:46:23 +0530

    Add Blink_bitwise, DC_motor, and DC_motor_function directories

---
+++ Codes/DC _Motor/DC _Motor/main.c
@@ -0,0 +1,39 @@
+/*
+ * DC _Motor.c
+ *
+ * Created: 1/27/2026 9:47:46 PM
+ * Author : acer cc
+ */ 
+#define F_CPU 16000000UL  //Defining crystal frequency
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB|=(1<<DDB4) | (1 << DDB5);       // Configuring PB4 AND PB5 as output
+	
+	while (1)
+	{
+		PORTB |=(1<<PB4);        // Making PB4 = HIGH
+		PORTB &=~(1 << PB5);   // PB5=LOW
+		_delay_ms(2000);        // 2 Second delay
+		
+		 // Motor Stop
+		 PORTB &=~(1 << PB4);    // PB4=LOW
+		 PORTB &=~(1 << PB5);   // PB5=LOW
+		 _delay_ms(2000);       // 2 seconds delay
+		 
+		 PORTB |=(1 << PB5);   // PB5=HIGH
+		 PORTB &=~(1<<PB4);    //  PB4 = LOW
+		_delay_ms(2000);       // 2 second delay
+		
+		 PORTB &=~(1 << PB4);  // PB4=LOW
+		 PORTB &=~(1 << PB5);  // PB5=LOW
+		 _delay_ms(2000);       // 2 seconds delay
+	}
+}
+
+
+
+
---
+++ Codes/DC_motor_function/DC_motor_function/main.c
@@ -0,0 +1,56 @@
+/*
+ * DC_motor_function.c
+ *
+ * Created: 1/28/2026 12:43:46 PM
+ * Author : acer cc
+ */ 
+
+
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+/* Function declarations */
+void cw(unsigned int delay);
+void ccw(unsigned int delay);
+void stp(unsigned int delay);
+
+int main(void)
+{
+    /* Set PB4 and PB5 as output */
+    DDRB |= (1 << DDB4) | (1 << DDB5);
+
+    while (1)
+    {
+        cw(2000);     // Rotate motor clockwise for 2 seconds
+        stp(2000);    // Stop motor for 2 seconds
+        ccw(2000);    // Rotate motor counter-clockwise for 2 seconds
+        stp(2000);    // Stop motor for 2 seconds
+    }
+}
+
+/* Clockwise rotation */
+void cw(unsigned int delay)
+{
+    PORTB |= (1 << PB4);      // PB4 = HIGH
+    PORTB &= ~(1 << PB5);     // PB5 = LOW
+    _delay_ms(delay);
+}
+
+/* Counter-clockwise rotation */
+void ccw(unsigned int delay)
+{
+    PORTB |= (1 << PB5);      // PB5 = HIGH
+    PORTB &= ~(1 << PB4);     // PB4 = LOW
+    _delay_ms(delay);
+}
+
+/* Motor stop */
+void stp(unsigned int delay)
+{
+    PORTB &= ~(1 << PB4);     // PB4 = LOW
+    PORTB &= ~(1 << PB5);     // PB5 = LOW
+    _delay_ms(delay);
+}
+
